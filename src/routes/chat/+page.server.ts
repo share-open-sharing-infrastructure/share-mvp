4f54edab358533e0c99795841a59d790714437e6
MaRaMinden <matt.ramin@gmail.com>
2025-11-12 22:47:25 +0100

    Dynamically fetch userId for chat

--- src/routes/chat/+page.server.ts
+++ src/routes/chat/+page.server.ts
@@ -6,9 +6,10 @@ export const load = (async ({ locals }) => {
     if (!locals.pb.authStore.record) {
             return redirect(303, '/login')
     }
+    const userId = locals.pb.authStore.record.id;
 
     const records = await locals.pb.collection('messages').getFullList({
-         filter: 'from = "uqszaef0c0r000q" || to = "uqszaef0c0r000q"',
+         filter: `from = "${userId}" || to = "${userId}"`,
     });
     
     return {
