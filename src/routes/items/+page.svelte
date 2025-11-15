85ce38f05edccfce05471491f588599b4609b123
I504010 <me@timojohner.de>
2025-11-15 22:41:03 +0100

    feat(item): add item creation modal and logic

--- src/routes/items/+page.svelte
+++ src/routes/items/+page.svelte
@@ -2,14 +2,28 @@
 	const { data } = $props();
 	const { items, uniqueNames, uniquePlaces } = data;
 	import { Section, TableHeader } from 'flowbite-svelte-blocks';
-	import { Card, Img, Gallery, Button, Search, Dropdown, Checkbox } from 'flowbite-svelte';
+	import {
+		Card,
+		Img,
+		Gallery,
+		Button,
+		Search,
+		Dropdown,
+		Checkbox,
+		Modal,
+		Label,
+		Input,
+		Fileupload,
+		Helper
+	} from 'flowbite-svelte';
 	import { ChevronDownOutline } from 'flowbite-svelte-icons';
 	import { selectedNames, selectedPlaces, searchTextState } from '../state.svelte';
 
+	let formModal = $state(false);
+	let error = $state('');
 
 	// helper for case insensitive search
-	const includesCaseInsensitive = (str, searchString) =>
-	new RegExp(searchString, 'i').test(str);
+	const includesCaseInsensitive = (str, searchString) => new RegExp(searchString, 'i').test(str);
 
 	let filterList = $derived.by(() => {
 		let filteredResults = [];
@@ -32,7 +46,7 @@
 				includesCaseInsensitive(items.name, searchTextState.value)
 			);
 		}
-		console.log(filteredResults);
+		// console.log(filteredResults);
 		return filteredResults;
 	});
 </script>
@@ -60,6 +74,7 @@
 			{/snippet}
 			<Button
 				class="bg-gray-800 text-white hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+				onclick={() => (formModal = true)}
 			>
 				Gegenstand hinzufügen
 			</Button>
@@ -79,24 +94,37 @@
 				</svg>
 				Filter<ChevronDownOutline />
 			</Button>
+
 			<Dropdown simple class="w-48 p-2 text-sm">
 				<h6 class="mb-1 ml-1 text-sm font-medium text-gray-900 dark:text-white">Ort</h6>
 				{#each uniquePlaces as place}
 					<li class="rounded p-1 hover:bg-gray-100 dark:hover:bg-gray-600">
-						<Checkbox checked inline class="text-gray-900 focus:ring-gray-700" bind:group={selectedPlaces.selectedValues} value={place}>{place}</Checkbox>
+						<Checkbox
+							checked
+							inline
+							class="text-gray-900 focus:ring-gray-700"
+							bind:group={selectedPlaces.selectedValues}
+							value={place}>{place}</Checkbox
+						>
 					</li>
 				{/each}
 				<h6 class="mt-5 mb-1 ml-1 text-sm font-medium text-gray-900 dark:text-white">Name</h6>
 				{#each uniqueNames as name}
 					<li class="rounded p-1 hover:bg-gray-100 dark:hover:bg-gray-600">
-						<Checkbox checked inline class="text-gray-900 focus:ring-gray-700" bind:group={selectedNames.selectedValues} value={name}>{name}</Checkbox>
+						<Checkbox
+							checked
+							inline
+							class="text-gray-900 focus:ring-gray-700"
+							bind:group={selectedNames.selectedValues}
+							value={name}>{name}</Checkbox
+						>
 					</li>
 				{/each}
 			</Dropdown>
 		</TableHeader>
 	</Section>
 
-	<div class="mx-auto max-w-6xl space-y-4 overflow-x-auto p-6 sm:p-8 md:space-y-6">
+	<div class="mx-auto max-w-6xl space-y-4 overflow-x-auto p-4 md:space-y-6">
 		<Gallery class="grid-cols-1 gap-4 md:grid-cols-4">
 			{#each filterList as item}
 				<Card>
@@ -131,3 +159,38 @@
 		</Gallery>
 	</div>
 </Section>
+
+<Modal form bind:open={formModal} size="xs">
+	<form
+		class="flex flex-col space-y-6"
+		action="?/create"
+		method="post"
+		enctype="multipart/form-data"
+	>
+		<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Details</h3>
+		{#if error}
+			<Label color="red">{error}</Label>
+		{/if}
+		<Label class="space-y-2">
+			<span>Bild hochladen</span>
+			<Fileupload type="file" id="with_helper" name="image" class="mb-2" />
+			<Helper>SVG, PNG, JPG or GIF (max. 800x400px).</Helper>
+		</Label>
+		<Label class="space-y-2">
+			<span>Name</span>
+			<Input type="text" name="name" placeholder="Name des Gegenstands" required />
+		</Label>
+		<Label class="space-y-2">
+			<span>Beschreibung</span>
+			<Input type="text" name="description" placeholder="Beschreibung des Gegenstands" required />
+		</Label>
+		<Label class="space-y-2">
+			<span>Ort</span>
+			<Input type="text" name="place" placeholder="Ort des Gegenstands" required />
+		</Label>
+		<Button
+			class="bg-gray-800 text-white hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+			type="submit">Gegenstand hinzufügen</Button
+		>
+	</form>
+</Modal>
