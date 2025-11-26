a10b214e883e05ab88cd391d58c62ab272e48767
MaRaMinden <matt.ramin@gmail.com>
2025-11-26 12:48:00 +0100

    Improve logout

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,12 +1,8 @@
-import { fail, redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
-export const load = (async ({ locals }) => {
+export function load ({ locals })  {
     if (locals.pb.authStore.record) { // Check if the user is authenticated
         locals.pb.authStore.clear(); // Clears the auth store, thereby effectively logs the user out
-        return redirect(303, '/');
+        redirect(303, '/');
     }
-
-    return {
-    };
-}) satisfies PageServerLoad;
+};
