b893cdad0f6a37873746393173b4c72849c25173
MaRaMinden <matt.ramin@gmail.com>
2025-12-03 18:13:47 +0100

    Add feedback button and embed Notion form

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,11 @@
 	import { goto, invalidateAll } from '$app/navigation';
 	import Flash from '$lib/Flash.svelte';
 	import '../app.css';
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, GradientButton, Modal } from 'flowbite-svelte';
 
 	let { children, data } = $props();
+
+	let feedbackModalNotion = $state(false);
 
 	async function logout() {
         const res = await fetch('/logout', {
@@ -41,8 +43,34 @@
 	</NavUl>
 </Navbar>
 
+<GradientButton
+	color="redToYellow" 
+	pill
+	onclick={() => {feedbackModalNotion = true}}
+			
+	class="
+		fixed               /* take it out of the normal flow */
+		bottom-10 left-10    /* position in the corner */
+		z-50                /* above other content */
+	">
+	Feedback geben
+</GradientButton>
+
 {#if data.flash}
   <Flash flash={data.flash} />
 {/if}
 
 {@render children()}
+
+<Modal bind:open={feedbackModalNotion} size="lg" title="Feedback geben">
+	<iframe 
+		title="Feedback Form"
+		src="https://dismantle-capitalism.notion.site/ebd/2bc20d17d64980d798b8f4d1105c8bc0" 
+		width="100%"
+		height="800"
+		frameborder="0" 
+		allowfullscreen 
+		>
+	</iframe>
+</Modal>
+
