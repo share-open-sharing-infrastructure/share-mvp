1aa025e247119c031e0d7d6168020325d58cbea8
MaRaMinden <matt.ramin@gmail.com>
2025-12-03 19:34:18 +0100

    Add footer

---
+++ src/routes/org/about/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { APP_NAME } from '$lib/names';
+</script>
+
+<h1 class="text-2xl font-bold mb-4">Über {APP_NAME}</h1>
+<div>
+    <p>{APP_NAME} ist eine gemeinnützige open-source Verleih-Plattform, die es Menschen ermöglicht, Gegenstände in ihrer Gemeinschaft zu teilen und zu verleihen. </p>
+    <br>
+    <p>Unser Ziel ist es, den Zugang zu Ressourcen zu erleichtern, die Umweltbelastung zu reduzieren und das Gemeinschaftsgefühl zu stärken.</p>
+    <br>
+    <p>
+        {APP_NAME} ist quelloffen und lebt von der Gemeinschaft. 
+        Den Code findest du auf <a href="https://github.com/share-open-sharing-infrastructure/share-mvp" class="underline">GitHub</a>.
+    </p>
+
+</div>
