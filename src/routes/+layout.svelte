113b9517a7b7690cec265db150e7442573358c1d
MaRaMinden <matt.ramin@gmail.com>
2025-12-08 19:30:14 +0100

    Fix a11y warnings in footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -84,12 +84,12 @@
     <FooterBrand href="#" src={share_logo} alt="share Logo" name="" aClass="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white" />
     <p class="my-6 text-gray-500 dark:text-gray-400">Ein gemeinnütziger open-source Verleih-Marktplatz. Für alle.</p>
     <FooterLinkGroup class="mb-6 flex flex-wrap items-center justify-center text-gray-900 dark:text-white">
-      <FooterLink aClass="mr-4 hover:underline md:mr-6" href="/org/about">Über</FooterLink>
-      <FooterLink aClass="mr-4 hover:underline md:mr-6" href="/org/imprint">Impressum</FooterLink>
-      <FooterLink aClass="mr-4 hover:underline md:mr-6" href="/org/contact">Kontakt</FooterLink>
+      <FooterLink classes={{ link: "mr-4 hover:underline md:mr-6" }} href="/org/about">Über</FooterLink>
+      <FooterLink classes={{ link: "mr-4 hover:underline md:mr-6" }} href="/org/imprint">Impressum</FooterLink>
+      <FooterLink classes={{ link: "mr-4 hover:underline md:mr-6" }} href="/org/contact">Kontakt</FooterLink>
     </FooterLinkGroup>
     <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-      © 2025-2026 <a href="#" class="hover:underline">{APP_NAME}™</a>
+      © 2025-2026 <a href="/" class="hover:underline">{APP_NAME}™</a>
       . All Rights Reserved.
     </span>
   </div>
