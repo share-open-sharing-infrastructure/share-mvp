f4d0fb369f6af037696a1fe0378c8b6af44928b0
MaRaMinden <matt.ramin@gmail.com>
2025-12-10 19:01:06 +0100

    Turn search page parts into components

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
 	const { data } = $props();
 	const { items, uniquePlaces } = data;
-	import { Section, TableHeader } from 'flowbite-svelte-blocks';
-	import { Gallery, Button, Search, Dropdown, Checkbox } from 'flowbite-svelte';
-	import { ChevronDownOutline } from 'flowbite-svelte-icons';
-	import ItemCard from './ItemCard.svelte';
+	import { Section } from 'flowbite-svelte-blocks';
 	import type { Item } from '$lib/types/models';
+	import SearchBar from './SearchBar.svelte';
+	import Welcome from './Welcome.svelte';
+	import Results from './Results.svelte';
 
 	let selectedPlaces: string[] = $state([]);
-	let searchText = $state('');
+	let searchText = $state({value: ''});
 
 	// Helper for case insensitive search
 	const includesCaseInsensitive = (str: string, searchString: string) =>
 		str.toLowerCase().includes(searchString.toLowerCase());
 
-	let isSearching: boolean = $derived(searchText.length > 0);
+	let isSearching: boolean = $derived(searchText.value.length > 0);
 
 	// Filters result set item list based on selected filters and search text
 	let filteredItemList: Item[] = $derived(
@@ -26,7 +26,7 @@
 			}
 
 			// Filter by search text
-			if (searchText.length && !includesCaseInsensitive(item.name, searchText)) {
+			if (searchText.value.length && !includesCaseInsensitive(item.name, searchText.value)) {
 				return false;
 			}
 
@@ -42,90 +42,21 @@
 		</span>
 	</div>
 
-	<Section
-		name="tableheader"
-		sectionClass="dark:bg-gray-900 flex pt-8 mx-auto max-w-5xl dark:hover:bg-gray-700"
-	>
-		<TableHeader headerType="search">
-			{#snippet search()}
-				<Search
-					size="md"
-					class="mr-4 flex flex-col"
-					classes={{ input: 'focus:ring-gray-700 focus:border-gray-700' }}
-					placeholder="Suche Dinge..."
-					bind:value={searchText}
-				/>
-			{/snippet}
-
-			{#if isSearching}
-				<Button color="light">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						aria-hidden="true"
-						class="mr-2 h-4 w-4 text-gray-400"
-						viewBox="0 0 20 20"
-						fill="currentColor"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-					Filter<ChevronDownOutline />
-				</Button>
-
-				<Dropdown simple class="w-48 p-2 text-sm">
-					<h6 class="mb-1 ml-1 text-sm font-medium text-gray-900 dark:text-white">Ort</h6>
-					{#each uniquePlaces as place}
-						<li class="rounded p-1 hover:bg-gray-100 dark:hover:bg-gray-600">
-							<Checkbox
-								checked
-								inline
-								class="text-gray-900 focus:ring-gray-700"
-								bind:group={selectedPlaces}
-								value={place}>{place}</Checkbox
-							>
-						</li>
-					{/each}
-				</Dropdown>
-			{/if}
-		</TableHeader>
-	</Section>
+	<SearchBar
+		searchText={searchText}
+		isSearching={isSearching}
+		selectedPlaces={selectedPlaces}
+		uniquePlaces={uniquePlaces}
+	/>
 
 	<div class="mx-auto max-w-5xl space-y-4 overflow-x-auto p-4 md:space-y-6">
 		{#if isSearching}
-			<div class="flex items-center justify-center pt-2">
-				<h5>{filteredItemList.length} Dinge gefunden</h5>
-			</div>
-			<Gallery
-				class="mx-auto max-w-5xl grid-cols-1 gap-4 pt-2 max-md:grid-cols-2 max-sm:grid-cols-3 md:grid-cols-1"
-			>
-				{#each filteredItemList as item}
-					<ItemCard
-						{item}
-						imgUrl={`${data.PB_IMG_URL}api/files/${item.collectionId}/${item.id}/${item.image}`}
-					/>
-				{/each}
-			</Gallery>
+			<Results 
+				filteredItemList={filteredItemList} 
+				PB_IMG_URL={data.PB_IMG_URL}
+			/>
 		{:else}
-			<div
-				class="
-				mx-auto
-				mt-10 flex max-w-md flex-col
-				items-center justify-center gap-4
-				text-center text-lg text-gray-600 dark:text-white
-				"
-			>
-				<p>Bei AllerLeih findest du allerlei Dinge aus deiner Umgebung</p>
-				<p>zum leihen, teilen, mieten, ...</p>
-				<p>Nutze einfach die Suche oben oder</p>
-				<Button
-					class="me-2 mb-2 w-full rounded-lg bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-					href="/profile">biete selbst etwas an!</Button
-				>
-				<p>von und für Freunde, Familie und die lokale Gemeinschaft</p>
-			</div>
+			<Welcome />
 		{/if}
 	</div>
 </Section>
