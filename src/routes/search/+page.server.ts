060735f5a293183daef505d31b89089a06399a8f
matteoramin <matt.ramin@gmail.com>
2025-12-10 18:32:58 +0100

    Fix form behaviors and clean up code (#77)
    * Enhance "add item" form behavior
    
    * Improve "edit item" form behavior (and re-introduce image updating, hupsi I deleted)
    
    * Remove unnecessary redirects
    
    * Improve documentation (proposal)
    
    * fix: display issue due to owner field name change

--- src/routes/search/+page.server.ts
+++ src/routes/search/+page.server.ts
@@ -1,9 +1,9 @@
 import { fail, redirect } from '@sveltejs/kit';
-import { PUBLIC_PB_URL } from '$env/static/public';
+import { PUBLIC_PB_URL } from '../../hooks.server';
 
 export async function load ({ locals }) {
 
-
+    // Prepare PocketBase query parameters. If user is logged in, filter out their own items.
     const pocketbaseQuery = {
         expand: 'owner',
         filter: locals.user ? `owner != "${locals.user.id}"` : undefined
@@ -41,35 +41,4 @@ export async function load ({ locals }) {
         uniquePlaces: structuredClone(uniquePlaces),
         userId: locals.user ? locals.user.id : null
     };
-};
-
-export const actions = {
-    create: async ({ locals, request }) => {
-        const data = await request.formData();
-        const name = data.get('name');
-        const description = data.get('description');
-        const place = data.get('place');
-        const image = data.get('image');
-        data.append('owner', locals.user.id);
-
-        const noImage = !image || !(image instanceof File) || image.size === 0 || !image.name;
-
-        if (!name || !description || !place || noImage ) {
-            return fail(400, {
-                fail: true,
-                nameRequired:name === null, 
-                descriptionRequired: description === null, 
-                placeRequired: place === null,
-                message: "Gegenstand konnte nicht hinzugefügt werden."
-            });
-        }
-
-        try {
-            await locals.pb.collection('items').create(data);
-        } catch (error) {
-            console.error(error?.message || error);
-        }
-        
-        redirect(303, '/');
-    }
 };
