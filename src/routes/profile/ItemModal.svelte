664a2ee0ae513695fd65affefe835471ab2d0d93
MaRaMinden <matt.ramin@gmail.com>
2025-12-15 13:22:45 +0100

    Cleanup

--- src/routes/profile/ItemModal.svelte
+++ src/routes/profile/ItemModal.svelte
@@ -1,20 +1,16 @@
 <script lang="ts">
-    import { Modal, Button, Input, Label, Fileupload, Helper, Toggle } from 'flowbite-svelte';
-    import { enhance } from '$app/forms';
+	import { Modal, Button, Input, Label, Fileupload, Helper, Toggle } from 'flowbite-svelte';
+	import { enhance } from '$app/forms';
 	import type { Item } from '$lib/types/models';
 
-	// This is still a bit suboptimal because we have to pass both editingItemId and editingItem from the parent even if we want to add a new item.
 	interface Props {
 		isVisible: boolean;
 		type: 'add' | 'edit';
 		editingItem: Item | null;
 	}
-
-    let {
-		isVisible = $bindable(), 
-		type,
-		editingItem
-	}: Props = $props();
+	
+	// This is still a bit suboptimal because we have to pass both editingItemId and editingItem from the parent even if we want to add a new item.
+	let { isVisible = $bindable(), type, editingItem }: Props = $props();
 </script>
 
 <Modal bind:open={isVisible} size="xs">
@@ -64,15 +60,20 @@
 			/>
 		</Label>
 		<Label class="space-y-2">
-			<Toggle name="trusteesOnly" checked={editingItem?.trusteesOnly ? editingItem.trusteesOnly : false}>Nur an Vertraute</Toggle>
+			<Toggle
+				name="trusteesOnly"
+				checked={editingItem?.trusteesOnly ? editingItem.trusteesOnly : false}
+				>Nur an Vertraute</Toggle
+			>
 		</Label>
 		<Button
 			class="bg-gray-800 text-white hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
 			type="submit"
 			onclick={() => {
 				isVisible = false;
-			}}>
-			{ type === 'edit' ? 'Speichern' : 'Hinzufügen' }
+			}}
+		>
+			{type === 'edit' ? 'Speichern' : 'Hinzufügen'}
 		</Button>
 	</form>
 	{#if type === 'edit'}
