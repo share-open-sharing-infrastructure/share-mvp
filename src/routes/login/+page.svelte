742b590854cd72eb29a82aa32970055faa716194
I504010 <timo.johner@sap.com>
2026-02-05 20:16:12 +0100

    fix register and login forms on mobile

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -11,7 +11,7 @@
 			Einloggen
 		{/snippet}
 		<div class="space-y-4 p-6 sm:p-8 md:space-y-6">
-			<form class="flex flex-col space-y-5" action="?/login" method="post">
+			<form class="flex flex-col space-y-5 m-5" action="?/login" method="post">
 				<h3 class="p-0 text-xl font-medium text-accent-900 dark:text-white">Wilkommen zurück!</h3>
 				{#if form?.fail}
 					<Alert>
