293a2269dce9bf9acaac6f590318111a406858dd
MaRaMinden <matt.ramin@gmail.com>
2026-02-10 13:20:50 +0100

    rm flash logic

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,18 +1,5 @@
 export const load = (event) => {
-	const flashCookie = event.cookies.get('flash');
-	let flash = null;
-
-	if (flashCookie) {
-		try {
-			flash = JSON.parse(flashCookie);
-		} catch (error) {
-			console.error('Failed to parse flash cookie:', error);
-		}
-		event.cookies.delete('flash', { path: '/' });
-	}
-
 	return {
-		flash,
 		currentUser: event.locals.user,
 	};
 };
