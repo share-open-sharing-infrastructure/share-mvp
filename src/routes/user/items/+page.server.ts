99f11cd3d5a2e687f3c37bb998107cdb0f31e38a
MaRaMinden <matt.ramin@gmail.com>
2026-04-11 22:19:53 +0200

    Ensure items to be 'available' per default after upload

--- src/routes/user/items/+page.server.ts
+++ src/routes/user/items/+page.server.ts
@@ -66,6 +66,7 @@ export const actions = {
 				image: formData.get('itemImage'),
 				owner: locals.user.id,
 				trusteesOnly: formData.get('trusteesOnly') === 'on' ? true : false,
+				status: 'available',
 			});
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		} catch (error: Error | any) {
