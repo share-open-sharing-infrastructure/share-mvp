cc81b615f29006d000c6ecfdfd0ef4024d37ec20
matteoramin <matt.ramin@gmail.com>
2026-04-14 11:05:28 +0200

    Update src/routes/misc/faq/+page.svelte
    Co-authored-by: Timo <32620814+timojohlo@users.noreply.github.com>

--- src/routes/misc/faq/+page.svelte
+++ src/routes/misc/faq/+page.svelte
@@ -19,9 +19,9 @@
 	<AccordionItem>
 		{#snippet header()}Wer seid ihr?{/snippet}
 		<p class="text-gray-500 dark:text-gray-400">
-			Aktuell sind wir Timo und Matteo! Wir sind eher Anfang als Mitte 30, haben beide in Lüneburg studiert und wollen mit AllerLeih einen Beitrag zum Gemeinwohl leisten.
+			Derzeit sind wir ein Duo: Timo und Matteo! Wir haben beide in Lüneburg studiert und wollen mit AllerLeih einen Beitrag zum Gemeinwohl leisten.
 			Wir sind der Auffassung, dass das Teilen und Leihen in vielerlei Hinsicht eine bessere Alternative zum Kaufen ist.
-			Und wir wollen, dass die Infrastruktur dafür nicht nur einfach und zugänglich ist, sondern auch öffentlicher Kontrolle unterworfen ist.
+			Und wir wollen, dass die Infrastruktur dafür nicht nur einfach und zugänglich ist, sondern auch nachhaltig für alle funktioniert.
 			Deswegen entwickeln wir AllerLeih als gemeinnützige Organisation und Open-Source-Software. So verhindern wir die Kommerzialisierung und manipulative Algorithmen.
 		</p>
 	</AccordionItem>
