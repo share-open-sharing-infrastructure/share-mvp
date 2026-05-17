50bb27802ec7826b9dbfe8bddeea2e6bc9c258db
MaRaMinden <matt.ramin@gmail.com>
2026-05-17 16:48:46 +0200

    Add unread indicator to navbar burger

--- src/lib/components/NavBarComponent.svelte
+++ src/lib/components/NavBarComponent.svelte
@@ -67,7 +67,12 @@
 			{texts.nav.translate}
 		</button>
 	{/if}
-	<NavHamburger />
+	<div class="relative">
+		<NavHamburger />
+		{#if unreadCount > 0}
+			<span class="absolute top-0.5 right-0.5 h-2.5 w-2.5 bg-red-500 rounded-full pointer-events-none xl:hidden"></span>
+		{/if}
+	</div>
 	<NavUl
 		{activeUrl}
 		classes={{
