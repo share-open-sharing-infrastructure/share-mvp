fde51122ee88d09be2610e22c9686b7a6b53112c
Timo <32620814+timojohlo@users.noreply.github.com>
2026-06-24 08:05:55 +0200

    Update texts.ts

--- src/lib/texts.ts
+++ src/lib/texts.ts
@@ -476,7 +476,7 @@ export const texts = {
 			sectionTitle: 'Benachrichtigungen',
 			description: 'Erhalte eine Benachrichtigung, wenn jemand deine Dinge anfragt oder dir schreibt.',
 			enable: 'Benachrichtigungen aktivieren',
-			enabled: 'Benachrichtigungen sind aktiviert. Du erhälst eine Benachrichtigung,wenn jemand deine Dinge anfragt oder dir schreibt.',
+			enabled: 'Benachrichtigungen sind aktiviert. Du erhälst eine Benachrichtigung, wenn jemand deine Dinge anfragt oder dir schreibt.',
 			deactivateThisDevice: 'Für dieses Gerät deaktivieren',
 			deactivateAllDevices: 'Für alle meine Geräte deaktivieren',
 			denied: 'Benachrichtigungen sind in deinem Browser blockiert. Du kannst sie in deinen Browser-Einstellungen wieder aktivieren.',
